de57e19619d2e439edb97c58df8c53169749d683
niel <sympta113@gmail.com>
2022-09-02 22:48:44 +0200

    ajout modif gamepass

--- Xbox GamePass/xbox.js
+++ Xbox GamePass/xbox.js
@@ -3,13 +3,16 @@ let etat = true;
 function myFunction() {
     var r = document.querySelector(':root');
     var element = document.getElementById("slidebar-container");
+    var btn = document.getElementById("menu-button");
     element.classList.toggle("toggled");
     
     if (etat) {
         r.style.setProperty('--heightSlide', '200px');
+        btn.style.paddingRight = "142px";
         etat = false;
     } else {
-        r.style.setProperty('--heightSlide', '70px');
+        r.style.setProperty('--heightSlide', '68px');
+        btn.style.paddingRight = "10px";
         etat = true;
     }
     
