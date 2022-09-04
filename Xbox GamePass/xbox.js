cdc50d22c4acee44f143f944f4e32446a9d7c68d
niel <sympta113@gmail.com>
2022-09-04 23:02:55 +0200

    modif xbox projet

--- Xbox GamePass/xbox.js
+++ Xbox GamePass/xbox.js
@@ -1,18 +1,49 @@
 document.getElementById("btn-menu").onclick = function() {myFunction()};
 let etat = true;
-
+document.getElementById("btn-menu").on
 function myFunction() {
     if (etat) {
         document.querySelector(':root').style.setProperty('--heightSlide', '200px');
+        var e = document.querySelectorAll("div.slidebar-top p");
+        for (let index = 0; index < e.length; index++) {
+            e[index].style.visibility = "visible";
+            
+        }
+        
         etat = false;
     } else {
         document.querySelector(':root').style.setProperty('--heightSlide', '70px');
+        var e = document.querySelectorAll("div.slidebar-top p");
+        for (let index = 0; index < e.length; index++) {
+            e[index].style.visibility = "hidden";
+            
+        }
         etat = true;
     }
-    
-    
+     
 }
 
+
+
+function mouseOver() {
+    document.querySelector(':root').style.setProperty('--heightSlide', '200px');
+    var e = document.querySelectorAll("div.slidebar-top p");
+    for (let index = 0; index < e.length; index++) {
+        e[index].style.visibility = "visible";
+        
+    }
+    etat = false;
+}
+  
+function mouseOut() {
+    document.querySelector(':root').style.setProperty('--heightSlide', '70px');
+    var e = document.querySelectorAll("div.slidebar-top p");
+    for (let index = 0; index < e.length; index++) {
+        e[index].style.visibility = "hidden";
+        
+    }
+    etat = true;
+}
 
 let l;
 let index;
