9c412900f4045e6091869a1bd75d3b8a915fb47b
niel <sympta113@gmail.com>
2022-09-08 03:45:06 +0200

    modif xbox projet

--- Xbox GamePass/xbox.js
+++ Xbox GamePass/xbox.js
@@ -75,18 +75,19 @@ var swiper = new Swiper(".mySwiper", {
 });
 
 document.getElementById("generator input[type=text]")
-
+let compteurDivJeux=0;
 function getInputValue() {
     var e = document.querySelector("#generator input[type=number]").value;
     var titre = document.querySelector("#generator input[type=text]").value;
-    console.log(e);
+    //console.log(e);
     
     if (parseInt(e) > 1) {
-        for (let index = 0; index < parseInt(e); index++) {
-            createJeux(titre);
+        for (let index = 0; index < parseInt(e); index++,compteurDivJeux++) {
+            createJeux(titre,compteurDivJeux);
         }
     } else {
-        createJeux(titre);
+        createJeux(titre,compteurDivJeux);
+        compteurDivJeux++
     }
     
     
@@ -97,15 +98,15 @@ function createJeux(titre) {
     var selectBk = document.getElementById('choix-bk');
     var valueDiv = selectDiv.options[selectDiv.selectedIndex].value;
     var valueBk = selectBk.options[selectBk.selectedIndex].value;
-    console.log(valueDiv);
+    //console.log(valueDiv);
 
     var matches = document.querySelector("div[id="+valueDiv+"] .game");
-   // console.log(matches);
+    //console.log(matches);
     var divJeux = document.createElement('div');
-    divJeux.setAttribute('class','jeux');
+    divJeux.setAttribute('class','jeux-'+compteurDivJeux);
+    divJeux.setAttribute('style','background-image: url(images/'+valueBk+')');
     matches.appendChild(divJeux);
 
-   
     
     //console.log(divJeux);
     var divCatégorie = document.createElement('div');
@@ -115,13 +116,81 @@ function createJeux(titre) {
     divNj.setAttribute('class','name-jeux');
     divJeux.appendChild(divNj);
 
-    
-    //console.log(divNj);
+
     var a = document.createElement('a');
     a.setAttribute('href','Xbox GamePass/xbox.html');
-    a.innerHTML = titre;
+    a.innerHTML = titre+" - "+(compteurDivJeux + 1);
     divNj.appendChild(a);
 
-    
-     
+    detectsJeux();
 }
+let initial = 0;
+function buttonNext(who) {
+    var selectDiv = document.querySelectorAll('#'+who.parentNode.parentNode.id+' div[class^="jeux-"]');
+    if (initial > -((selectDiv.length*180)-(selectDiv.length%7)*180)) {
+        initial -= 180 * 7;
+        for (let index = 0; index < selectDiv.length; index++) {
+            selectDiv[index].style.transform = "translateX(" + initial + "px)";
+            
+            
+        } 
+   }
+    
+    
+}
+
+function buttonPrec(who) {
+    var selectDiv = document.querySelectorAll('#'+who.parentNode.parentNode.id+' div[class^="jeux-"]');
+    if (initial < 0) {
+        initial += 180 * 7;
+        for (let index = 0; index < selectDiv.length; index++) {
+            selectDiv[index].style.transform = "translateX(" + initial + "px)";
+            
+        }
+    }
+    
+}
+
+function detectJeux(div) {
+    var selectDiv = document.querySelector(' #'+div+' div[class^="jeux-"]');
+    if (selectDiv) {
+        var selectButton = document.querySelectorAll('#'+div+' .pagination input');
+    
+        for (let index = 0; index < selectButton.length; index++) {
+            selectButton[index].visibility = 'visible'
+            
+        }
+    }
+    
+}
+
+function detectsJeux() {
+    console.log("////////////////////////");
+    var selectDivAJ = document.querySelectorAll('div[id^="ajout-container-"]');
+
+    
+
+    for (let index = 0; index < selectDivAJ.length; index++) {
+        var selectDiv = document.querySelector('div[id^="ajout-container-'+(index+1)+'"] div[class^="jeux-"]');
+        console.log(selectDiv);
+        
+        if (selectDiv) {
+            console.log("il ya des div Jeux");
+            var selectButton = document.querySelectorAll('div[id^="ajout-container-'+(index+1)+'"] .pagination input');
+
+            for (let index = 0; index < selectButton.length; index++) {
+                console.log("button : "+index+" visible");
+                selectButton[index].style.visibility = 'visible'
+                
+            }
+        }
+
+        
+       
+        
+    }
+    
+    
+    console.log("////////////////////////");
+}
+detectsJeux();
